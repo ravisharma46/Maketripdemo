cd6fd358c1182af63c675281b38ff73a57cdcfc3
ravisharma46 <ravi.ravi.sharma46@gmail.com>
2019-06-11 10:11:28 +0530

    Holidayinactivity

--- app/src/main/java/com/example/apple/maketripdemo/HolidayInActivity.java
+++ app/src/main/java/com/example/apple/maketripdemo/HolidayInActivity.java
@@ -1,9 +1,12 @@
 package com.example.apple.maketripdemo;
 
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 
 public class HolidayInActivity extends AppCompatActivity {
 
@@ -13,7 +16,12 @@ public class HolidayInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_holiday_in);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#268adf")));
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#000000'>Holiday in Bhutan</font>"));
+
+        final Drawable upArrow = getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp);
+        upArrow.setColorFilter(getResources().getColor(R.color.black), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
     }
 }
