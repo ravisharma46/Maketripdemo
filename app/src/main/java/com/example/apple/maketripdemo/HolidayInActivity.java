220b842cb75a98d5f2b2570d46eb7c974d9754e2
ravisharma46 <ravi.ravi.sharma46@gmail.com>
2019-06-12 13:14:39 +0530

    add call button

--- app/src/main/java/com/example/apple/maketripdemo/HolidayInActivity.java
+++ app/src/main/java/com/example/apple/maketripdemo/HolidayInActivity.java
@@ -1,19 +1,42 @@
 package com.example.apple.maketripdemo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HolidayInActivity extends AppCompatActivity {
+
+    public Button bt;
+    private LinearLayout mLinear;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_holiday_in);
+
+        bt= (Button) findViewById(R.id.fab_call);
+        mLinear = (LinearLayout) findViewById(R.id.child_linear);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
@@ -22,6 +45,63 @@ public class HolidayInActivity extends AppCompatActivity {
         final Drawable upArrow = getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp);
         upArrow.setColorFilter(getResources().getColor(R.color.black), PorterDuff.Mode.SRC_ATOP);
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                bt.setVisibility(View.INVISIBLE);
+
+                BottomSheetDialog dialog = new BottomSheetDialog(HolidayInActivity.this);
+                View sheetView = HolidayInActivity.this.getLayoutInflater().inflate(R.layout.call_user, null);
+                dialog.setContentView(sheetView);
+                dialog.setCanceledOnTouchOutside(true);
+                dialog.show();
+
+                TextView query = (TextView) sheetView.findViewById(R.id.send_query);
+                TextView call = (TextView) sheetView.findViewById(R.id.call);
+
+                // Set  query button click listener
+                query.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Dismiss the alert dialog
+
+                        Toast.makeText(getApplication(),
+                                "yes button clicked", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+
+               // Set  call button click listener
+                call.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                        callIntent.setData(Uri.parse("tel:123456789"));
+                        startActivity(callIntent);
+                    }
+                });
+
+                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        bt.setVisibility(View.VISIBLE);
+                    }
+                });
+
+
+
+
+
+            }
+        });
+
+
+
+
 
     }
 }
